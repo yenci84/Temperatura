0bbd2f0a066cec5e414e0525bd2e3255e2aaa891
yenci84 <107704483+yenci84@users.noreply.github.com>
2022-06-17 11:19:34 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,10 @@ basic.forever(function () {
     basic.showNumber(input.temperature())
     if (input.temperature() > 25) {
         basic.showString("Hot")
+    } else if (input.temperature() < 15) {
+        basic.showString("Cold")
     } else {
-    	
+        basic.showIcon(IconNames.Happy)
+        basic.showString("Temperatura Agradable")
     }
 })
