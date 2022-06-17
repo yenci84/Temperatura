1ba9aa2241f88dae39355c9883377e7c4fd1570f
yenci84 <107704483+yenci84@users.noreply.github.com>
2022-06-17 11:21:08 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 basic.forever(function () {
     basic.showNumber(input.temperature())
     if (input.temperature() > 25) {
-        basic.showString("Hot")
+        basic.showString("HOT")
+        music.playTone(262, music.beat(BeatFraction.Whole))
     } else if (input.temperature() < 15) {
-        basic.showString("Cold")
+        basic.showString("COLD")
+        music.playTone(392, music.beat(BeatFraction.Whole))
     } else {
         basic.showIcon(IconNames.Happy)
         basic.showString("Temperatura Agradable")
